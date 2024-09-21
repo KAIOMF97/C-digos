3048d546849cd48c61bde8365ac5bba6d1b3f5fa
KAIOMF97 <kaiomangolinfernandes@alunos.utfpr.edu.br>
2024-09-21 10:51:55 -0300

    Add files via upload

---
+++ AlunosMVC/alunoClass.js
@@ -0,0 +1,10 @@
+class Aluno {
+    constructor(ra, nome, nota1b, nota2b, nota3b, nota4b){
+        this.ra = ra;
+        this.nome = nome;
+        this.nota1b = nota1b;
+        this.nota2b = nota2b;
+        this.nota3b = nota3b;
+        this.nota4b = nota4b;
+    }
+}
